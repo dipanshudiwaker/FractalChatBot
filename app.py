5c9dcafbef790dfc08cb04d438c3b02e0eb1b189
Dipanshu Diwaker <56470089+dipanshudiwaker@users.noreply.github.com>
2023-05-04 10:43:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import os
+from flask import Flask, render_template, request
 import openai
 from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
+
+load_dotenv(find_dotenv()) # read local .env file
 
 openai.organization = "org-Ju24Bfv0d8D7PHiH9INX976p"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
-# In[2]:
-
+app = Flask(__name__)
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
@@ -31,87 +25,49 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
         messages=messages,
         temperature=temperature, # this is the degree of randomness of the model's output
     )
-#     print(str(response.choices[0].message))
     return response.choices[0].message["content"]
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# In[3]:
-
-
-def collect_messages(_):
-    prompt = inp.value_input
-    inp.value = ''
-    context.append({'role':'user', 'content':f"{prompt}"})
+@app.route('/chat', methods=['POST'])
+def chat():
+    context = [ {'role':'system', 'content':"""
+        You are OrderBot, an automated service to collect orders for a pizza restaurant. \
+        You first greet the customer, then collects the order, \
+        and then asks if it's a pickup or delivery. \
+        You wait to collect the entire order, then summarize it and check for a final \
+        time if the customer wants to add anything else. \
+        If it's a delivery, you ask for an address. \
+        Finally you collect the payment.\
+        Make sure to clarify all options, extras and sizes to uniquely \
+        identify the item from the menu.\
+        You respond in a short, very conversational friendly style. \
+        The menu includes \
+        pepperoni pizza  12.95, 10.00, 7.00 \
+        cheese pizza   10.95, 9.25, 6.50 \
+        eggplant pizza   11.95, 9.75, 6.75 \
+        fries 4.50, 3.50 \
+        greek salad 7.25 \
+        Toppings: \
+        extra cheese 2.00, \
+        mushrooms 1.50 \
+        sausage 3.00 \
+        canadian bacon 3.50 \
+        AI sauce 1.50 \
+        peppers 1.00 \
+        Drinks: \
+        coke 3.00, 2.00, 1.00 \
+        sprite 3.00, 2.00, 1.00 \
+        bottled water 5.00 \
+        """} ]  # accumulate messages
+    
+    message = request.form['message']
+    context.append({'role':'user', 'content':message})
     response = get_completion_from_messages(context) 
-    context.append({'role':'assistant', 'content':f"{response}"})
-    panels.append(
-        pn.Row('User:', pn.pane.Markdown(prompt, width=600)))
-    panels.append(
-        pn.Row('Assistant:', pn.pane.Markdown(response, width=600, style={'background-color': '#F6F6F6'})))
- 
-    return pn.Column(*panels)
+    context.append({'role':'assistant', 'content':response})
+    return {'response': response}
 
-
-# In[5]:
-
-
-
-
-
-# In[6]:
-
-
-import panel as pn  # GUI
-pn.extension()
-
-panels = [] # collect display 
-
-context = [ {'role':'system', 'content':"""
-You are OrderBot, an automated service to collect orders for a pizza restaurant. \
-You first greet the customer, then collects the order, \
-and then asks if it's a pickup or delivery. \
-You wait to collect the entire order, then summarize it and check for a final \
-time if the customer wants to add anything else. \
-If it's a delivery, you ask for an address. \
-Finally you collect the payment.\
-Make sure to clarify all options, extras and sizes to uniquely \
-identify the item from the menu.\
-You respond in a short, very conversational friendly style. \
-The menu includes \
-pepperoni pizza  12.95, 10.00, 7.00 \
-cheese pizza   10.95, 9.25, 6.50 \
-eggplant pizza   11.95, 9.75, 6.75 \
-fries 4.50, 3.50 \
-greek salad 7.25 \
-Toppings: \
-extra cheese 2.00, \
-mushrooms 1.50 \
-sausage 3.00 \
-canadian bacon 3.50 \
-AI sauce 1.50 \
-peppers 1.00 \
-Drinks: \
-coke 3.00, 2.00, 1.00 \
-sprite 3.00, 2.00, 1.00 \
-bottled water 5.00 \
-"""} ]  # accumulate messages
-
-
-inp = pn.widgets.TextInput(value="Hi", placeholder='Enter text here…')
-button_conversation = pn.widgets.Button(name="Chat!")
-
-interactive_conversation = pn.bind(collect_messages, button_conversation)
-
-dashboard = pn.Column(
-    inp,
-    pn.Row(button_conversation),
-    pn.panel(interactive_conversation, loading_indicator=True, height=300),
-)
-
-dashboard
-
-
-# In[ ]:
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
