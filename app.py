75216f8af1d547eb9062311809050431ca1ed22c
Dipanshu Diwaker <56470089+dipanshudiwaker@users.noreply.github.com>
2023-05-11 22:33:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def extract_email(string):
     match = re.search(pattern, string)
     keywords = ['fractal latest news', ' fractal news']
     pattern = r"\b(?:{})\b".format("|".join(map(re.escape, keywords)))
-    matches = re.findall(pattern, text, flags=re.IGNORECASE)
+    matches = re.findall(pattern, string, flags=re.IGNORECASE)
     if match:
         return match.group()
     elif matches:
