0f78fc0447bb94df815793d541d97ce7c912a263
Dipanshu Diwaker <56470089+dipanshudiwaker@users.noreply.github.com>
2023-05-11 14:02:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,11 +40,11 @@ def extract_email(string):
 def gettoken():
     url = "https://login.salesforce.com/services/oauth2/token"
     payload = {
-	     'client_id': '3MVG9fe4g9fhX0E4rB1MeKF0UTUC0MIyoSgh1s93CRKKtf0Jqt2Tu7087Isfn2kAFc._.530IW.XtK3lowSxk',
-             'client_secret': 'BA2470D1D4FF3761AA981A03773AC01547A5E7CBA35B116247702F858902B302',
-	     'username': 'dipanshu@aethereus.com',
-	     'password': '112001Dip#@kay6opTZTXbwtnwgepw0mJoyi',
-	     'grant_type': 'password'
+     'client_id': '3MVG9fe4g9fhX0E4rB1MeKF0UTUC0MIyoSgh1s93CRKKtf0Jqt2Tu7087Isfn2kAFc._.530IW.XtK3lowSxk',
+     'client_secret': 'BA2470D1D4FF3761AA981A03773AC01547A5E7CBA35B116247702F858902B302',
+     'username': 'dipanshu@aethereus.com',
+     'password': '112001Dip#@kay6opTZTXbwtnwgepw0mJoyi',
+     'grant_type': 'password'
            }
     files=[
           ]
@@ -52,6 +52,7 @@ def gettoken():
     response = requests.request("POST", url, headers=headers, data=payload, files=files)
     print(response["access_token"])
     return response["access_token"]
+
 def createprospect(email1):
     print(email1)
     api = gettoken()
